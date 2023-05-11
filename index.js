cc9d2631057e5cfc55d96ba94537100eb6cdb430
Rizky Pratama <104710750+Aruh1@users.noreply.github.com>
2023-05-11 08:11:08 +0000

    sard repair and apps for slash

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const config = require("./config.js");
 if (config.shardManager.shardStatus == true) {
   const { ShardingManager } = require("discord.js");
   const manager = new ShardingManager("./bot.js", {
+    totalShards: "auto",
     token: config.TOKEN || process.env.TOKEN,
+    timeout: -1,
+    respawn: true
   });
   manager.on("shardCreate", (shard) =>
     console.log(`Launched shard ${shard.id}`)
   );
-  manager.spawn();
+  manager.spawn({ amount: 'auto', delay:5500, timeout: 30000 }).catch(e => console.log(e))
+  // manager.spawn();
 } else {
   require("./bot.js");
 }
