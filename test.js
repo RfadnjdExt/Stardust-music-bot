5538ab3cc6c9858e7acaf823ea1dd0cdba5dbc2b
RfadnjdExt <raficanggih@gmail.com>
2023-05-27 20:40:08 +0700

    update test js

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 const fs = require("node:fs/promises");
+const shellQuote = require("shell-quote");
 const childProcess = require("node:child_process");
 
 (async () => {
     const configSampleJS = await fs.readFile("config.sample.js", "utf-8");
-    console.log(configSampleJS.replaceAll('\n', '\\n'));
+    console.log(shellQuote.quote([configSampleJS]).replaceAll('\n', '\\n'));
     // const proc = childProcess.spawn("printf", [configSampleJS]);
     // proc.stdout.pipe(process.stdout);
     // proc.stderr.pipe(process.stderr);
